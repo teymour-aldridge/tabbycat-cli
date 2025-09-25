740393a68a996d6f4c49541b08c8c40075624161
Teymour Aldridge <teymour@reasoning.page>
2025-09-25 14:22:39 +0100

    Add test sequence against a Tabbycat instance.

---
+++ tests/main.rs
@@ -0,0 +1,194 @@
+use std::collections::HashMap;
+use std::env;
+use std::process::Command;
+
+use serde::Serialize;
+use serde_json::json;
+
+#[derive(Serialize, serde::Deserialize, Clone)]
+pub struct Auth {
+    tabbycat_url: String,
+    tournament_slug: String,
+    api_key: String,
+}
+
+#[test]
+fn test_tabbycat_setup() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_timer(tracing_subscriber::fmt::time::uptime())
+        .with_target(false)
+        .with_ansi(true)
+        .init();
+
+    Command::new("cargo")
+        .args(["install", "--path", "."])
+        .status()
+        .expect("Failed to install package");
+
+    // Clone the repository
+    Command::new("git")
+        .args([
+            "clone",
+            "--depth=1",
+            "https://github.com/tabbycatDebate/tabbycat",
+        ])
+        .status()
+        .expect("Failed to clone repository");
+
+    // Change to tabbycat directory and run docker-compose up
+    env::set_current_dir("tabbycat").expect("Failed to change directory");
+    Command::new("docker-compose")
+        .args(["-f", "docker-compose.yml", "up", "--detach"])
+        .status()
+        .expect("Failed to start docker containers");
+
+    tracing::trace!("Running reset_db");
+
+    Command::new("docker-compose")
+        .args([
+            "run",
+            "web",
+            "python",
+            "tabbycat/manage.py",
+            "reset_db",
+            "--no-input",
+        ])
+        .status()
+        .expect("Failed to reset database");
+
+    tracing::trace!("Finished reset_db");
+
+    Command::new("docker-compose")
+        .args([
+            "run",
+            "web",
+            "python",
+            "tabbycat/manage.py",
+            "migrate",
+            "--no-input",
+        ])
+        .status()
+        .expect("Failed to reset database");
+
+    // Create superuser
+    let mut env_vars = HashMap::new();
+    env_vars.insert("DJANGO_SUPERUSER_PASSWORD".to_string(), "test".to_string());
+    env_vars.insert(
+        "DJANGO_SUPERUSER_EMAIL".to_string(),
+        "email@example.com".to_string(),
+    );
+    env_vars.insert("DJANGO_SUPERUSER_USERNAME".to_string(), "user".to_string());
+
+    Command::new("docker-compose")
+        .args([
+            "run",
+            "--env",
+            &format!(
+                "DJANGO_SUPERUSER_PASSWORD={}",
+                env_vars["DJANGO_SUPERUSER_PASSWORD"]
+            ),
+            "--env",
+            &format!(
+                "DJANGO_SUPERUSER_EMAIL={}",
+                env_vars["DJANGO_SUPERUSER_EMAIL"]
+            ),
+            "--env",
+            &format!(
+                "DJANGO_SUPERUSER_USERNAME={}",
+                env_vars["DJANGO_SUPERUSER_USERNAME"]
+            ),
+            "web",
+            "python",
+            "tabbycat/manage.py",
+            "createsuperuser",
+            "--noinput",
+        ])
+        .status()
+        .expect("Failed to create superuser");
+
+    // Get auth token
+    let output = Command::new("docker-compose")
+        .args([
+            "run",
+            "web",
+            "python",
+            "tabbycat/manage.py",
+            "dumpdata",
+            "authtoken.token",
+        ])
+        .output()
+        .expect("Failed to get auth token");
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let json: serde_json::Value =
+        serde_json::from_str(stdout.lines().last().unwrap().trim()).unwrap();
+    let api_key = json[0]["pk"].as_str().unwrap();
+
+    let home_dir = dirs::home_dir().expect("Could not determine home directory");
+    let auth_path = home_dir.join(".tabbycat");
+
+    std::fs::write(
+        auth_path,
+        toml::to_string(&Auth {
+            tabbycat_url: "http://localhost:8000".to_string(),
+            tournament_slug: "bp88team".to_string(),
+            api_key: api_key.to_string(),
+        })
+        .unwrap(),
+    )
+    .unwrap();
+
+    attohttpc::post("http://localhost:8000/api/v1/tournaments")
+        .header("Authorization", format!("Token {api_key}"))
+        .json(&json!({
+            "name": "bp88team",
+            "slug": "bp88team"
+        }))
+        .unwrap()
+        .send()
+        .unwrap();
+
+    env::set_current_dir("..").expect("Failed to change back to original directory");
+    Command::new("tabbycat")
+        .args([
+            "import",
+            "--judges-csv",
+            "data/judges.csv",
+            "--teams-csv",
+            "data/teams.csv",
+            "--institutions-csv",
+            "data/institutions.csv",
+            "--clashes-csv",
+            "data/clashes.csv",
+        ])
+        .status()
+        .expect("Failed to import data");
+
+    let teams: Vec<tabbycat_api::types::Team> =
+        attohttpc::get("http://localhost:8000/api/v1/tournaments/bp88team/teams")
+            .header("Authorization", format!("Token {api_key}"))
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+    assert_eq!(teams.len(), 88);
+
+    let speakers: Vec<tabbycat_api::types::Speaker> =
+        attohttpc::get("http://localhost:8000/api/v1/tournaments/bp88team/speakers")
+            .header("Authorization", format!("Token {api_key}"))
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+    assert_eq!(speakers.len(), 88 * 2);
+
+    let judges: Vec<tabbycat_api::types::Adjudicator> =
+        attohttpc::get("http://localhost:8000/api/v1/tournaments/bp88team/adjudicators")
+            .header("Authorization", format!("Token {api_key}"))
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+    assert_eq!(judges.len(), 80);
+}
